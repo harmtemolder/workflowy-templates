cc653d9502699d3ee65f69f658ea36c34b617f59
Harm <mail@harmtemolder.nl>
2019-06-12 15:35:30 +0200

    Added cookie to modal so it's only shown once every 365 days

--- calendar-generator/public/scripts/modal-functions.js
+++ calendar-generator/public/scripts/modal-functions.js
@@ -1,5 +1,34 @@
 // modal-functions.js
 
+const cookieName = 'warning_modal_shown';
+
+function getCookie(cname) {
+  const name = `${cname}=`;
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const ca = decodedCookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return '';
+}
+
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  const expires = `expires=${d.toUTCString()}`;
+  document.cookie = `${cname}=${cvalue};${expires};path=/`;
+}
+
+function setModalCookie(cname) {
+  setCookie(cname, 'true', 365);
+}
+
 function pushDataLayerEvent(eventType, eventCategory, eventAction, eventLabel,
   eventNonInteraction) {
   if (window.dataLayer != null) {
@@ -22,37 +51,48 @@ function pushCloseModalEvent(eventLabel) {
   );
 }
 
-window.addEventListener('DOMContentLoaded', () => {
-  const modal = document.getElementById('warning-modal');
-  const closeButton = document.getElementsByClassName('modal-close')[0];
+if (getCookie(cookieName) === '') {
+  window.addEventListener('DOMContentLoaded', () => {
+    const modalOverlay = document.getElementById('modal-overlay');
+    const modalDarken = document.getElementById('modal-darken');
+    const closeButton = document.getElementsByClassName('modal-close')[0];
 
-  // When the user clicks on the X, close the modal
-  closeButton.onclick = function clickedModalClose() {
-    modal.style.display = 'none';
-    pushCloseModalEvent('by clicking on the X');
-  };
+    modalOverlay.style.display = 'flex';
 
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function clickedOutsideModal(event) {
-    if (event.target === modal) {
-      modal.style.display = 'none';
-      pushCloseModalEvent('by clicking outside of the modal');
-    }
-  };
+    // When the user clicks on the X, close the modal
+    closeButton.onclick = function clickedModalClose(event) {
+      modalOverlay.style.display = 'none';
+      setModalCookie(cookieName);
+      pushCloseModalEvent('by clicking on the X');
 
-  // When the user clicks on one of the buttons in the modal, close it
-  window.onclick = function clickedModalButton(event) {
-    if (event.target.classList.contains('modal-button')) {
-      if (event.target.classList.contains('new-user')) {
-        modal.style.display = 'none';
-        pushCloseModalEvent('by clicking the "new user" button');
-      } else if (event.target.classList.contains('free-user')) {
-        modal.style.display = 'none';
-        pushCloseModalEvent('by clicking the "free user" button');
-      } else if (event.target.classList.contains('pro-user')) {
-        modal.style.display = 'none';
-        pushCloseModalEvent('by clicking the "pro user" button');
+      event.preventDefault();
+    };
+
+    window.onclick = function clickedModalButton(event) {
+      if (event.target === modalDarken) {
+        // When the user clicks anywhere outside of the modal, close it
+
+        modalOverlay.style.display = 'none';
+        setModalCookie(cookieName);
+        pushCloseModalEvent('by clicking outside of the modal');
+
+        event.preventDefault();
+      } else if (event.target.classList.contains('modal-button')) {
+        // When the user clicks on one of the buttons in the modal, close it
+
+        modalOverlay.style.display = 'none';
+        setModalCookie(cookieName);
+
+        if (event.target.classList.contains('new-user')) {
+          pushCloseModalEvent('by clicking the "new user" button');
+        } else if (event.target.classList.contains('free-user')) {
+          pushCloseModalEvent('by clicking the "free user" button');
+        } else if (event.target.classList.contains('pro-user')) {
+          pushCloseModalEvent('by clicking the "pro user" button');
+        }
+
+        event.preventDefault();
       }
-    }
-  };
-});
+    };
+  });
+}
