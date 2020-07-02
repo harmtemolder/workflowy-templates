f8f4daf3f607104ac4629edc0435751884371c5e
Harm <mail@harmtemolder.nl>
2020-07-02 07:30:16 -0400

    Tweak setup.sh

--- python/setup.sh
+++ python/setup.sh
@@ -5,8 +5,13 @@
 # conda activate ./env
 
 # Install dependencies through `pip`
-virtualenv env
-source ./env
+python3 -m venv env
+source env/bin/activate
 # pip install -r requirements.txt
-# Install line by line, ignoring versions, because some packages I installed through `conda` are not available on `pip`:
+# Install line by line instead, ignoring versions, because some packages I installed through `conda` are not available on `pip`:
 cat requirements.txt | sed 's/[= ].*//' | xargs -n 1 pip install
+
+# Set script files executable
+chmod +x start.sh
+chmod +x debug.sh
+chmod +x update-setup.sh
